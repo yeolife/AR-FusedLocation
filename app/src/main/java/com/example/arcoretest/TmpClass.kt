702fa1621123a650a8098f4f1c29edd619a6faff
yeo2507 <yeo2507@naver.com>
2024-09-06 17:14:16 +0900

    임시 커밋

---
+++ app/src/main/java/com/example/arcoretest/TmpClass.kt
@@ -0,0 +1,40 @@
+package com.example.arcoretest
+
+class TmpClass {
+    fun tmp() {
+        // 첫번째 수평 평면 감지
+//                    if (childNodes.isEmpty()) {
+//                        updatedFrame.getUpdatedPlanes()
+//                            .firstOrNull { it.type == Plane.Type.HORIZONTAL_UPWARD_FACING }
+//                            ?.let { it.createAnchorOrNull(it.centerPose) }?.let { anchor ->
+//                                childNodes += createAnchorNode(
+//                                    engine = engine,
+//                                    modelLoader = modelLoader,
+//                                    materialLoader = materialLoader,
+//                                    modelInstances = modelInstances,
+//                                    anchor = anchor
+//                                )
+//                            }
+//                    }
+
+//    if (node == null) {
+//        val hitResults = frame?.hitTest(motionEvent.x, motionEvent.y)
+//        hitResults?.firstOrNull {
+//            it.isValid(
+//                depthPoint = false,
+//                point = false
+//            )
+//        }?.createAnchorOrNull()
+//            ?.let { anchor ->
+//                planeRenderer = false
+//                childNodes += createAnchorNode(
+//                    engine = engine,
+//                    modelLoader = modelLoader,
+//                    materialLoader = materialLoader,
+//                    modelInstances = modelInstances,
+//                    anchor = anchor
+//                )
+//            }
+//    }
+    }
+}
